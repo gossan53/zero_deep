2f852ef40f5b1948852b3e67f5e421369fd4360a
unknown <>
2016-11-07 11:29:55 +0900

    add ReLU function to activation_function.py

--- ch03/activation_function.py
+++ ch03/activation_function.py
@@ -7,9 +7,16 @@ def step_function(x):
 def sigmoid(x):
 	return 1/(1+ np.exp(-x))
 
-x=np.arange(-5.0,5.0,0.1)
-#y=step_function(x)
-y=sigmoid(x)
-plt.plot(x,y)
-plt.ylim(-0.1,1.1)
-plt.show()
+def relu(x):
+	return np.maximum(0,x)
+
+if __name__ == '__main__':
+
+	x=np.arange(-5.0,5.0,0.1)
+	#y=step_function(x)
+	#y=sigmoid(x)
+	y=relu(x)
+	plt.plot(x,y)
+	plt.ylim(-0.1,5.1)
+	plt.show()
+
