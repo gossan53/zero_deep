33e7fd768c969a1654f2e56dde1032758352b067
goto <goto@bi.cs.titech.ac.jp>
2016-11-13 00:46:48 +0900

    Add softmax function to activation_function.py

--- ch03/activation_function.py
+++ ch03/activation_function.py
@@ -13,6 +13,13 @@ def relu(x):
 def identity_function(x):
 	return x
 
+def softmax(a):
+	c=np.max(a)
+	exp_a=np.exp(a-c)
+	sum_exp_a=np.sum(exp_a)
+	y=exp_a/sum_exp_a
+	return y
+
 if __name__ == '__main__':
 
 	x=np.arange(-5.0,5.0,0.1)
